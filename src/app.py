32bd5b289f57f299b0e4ba44959fb94b9c97df3e
Gaurav Bangalore <gbangalo@akamai.com>
2023-05-15 16:35:32 +0530

    update app.py

--- src/app.py
+++ src/app.py
@@ -13,7 +13,7 @@ def fetchDetails():
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World , you just opened the flask app!</p>"
+    return "<p>Hello, World , you just opened the flask demo app!</p>"
 
 @app.route("/health")
 def health():
